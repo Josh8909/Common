7f0ace26d8f514be81ed929bc4bf124b613f4416
Josh8909 <josh.luo8909@gmail.com>
2015-09-29 18:11:46 +0800

    define the detail for toFix

--- js/common.js
+++ js/common.js
@@ -1,6 +1,19 @@
 /*
   This is the common js file
- */
- 
-function fixNum (num, len) {
-}
+  */
+
+  function toFixed (origin, len, prefix) {
+  	origin = origin.toString();
+  	if (origin.length > len) 
+  		return origin.substr(origin.length - len);
+  	else if (origin.length == len) 
+  		return origin;
+  	else {
+	  	var result = '';
+	  	for (var i = 0, max = len - origin.length; i < max; i++) {
+	  		result += prefix;
+	  	}		
+	  	result += origin;
+	  	return result;
+  	}
+  }
