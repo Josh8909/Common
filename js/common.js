0e14457ad8438d6e9fceb5ec9bd47efadc4927c6
Josh8909 <josh.luo8909@gmail.com>
2015-10-13 16:59:14 +0800

    Add new common method for js extend

--- js/common.js
+++ js/common.js
@@ -1,21 +1,51 @@
-/*
+z/*
  * 格式化指定的对象<Number or String>
  * @param origin 数据
  *        len    数据长度
  *        prefix 补全字符
  */
-function toFixed (origin, len, prefix) {
-	origin = origin.toString();
-	if (origin.length > len) 
-		return origin.substr(origin.length - len);
-	else if (origin.length == len) 
-		return origin;
-	else {
-		var result = '';
-		for (var i = 0, max = len - origin.length; i < max; i++) {
-			result += prefix;
-		}		
-		result += origin;
-		return result;
-	}
-}
+ function toFixed (origin, len, prefix) {
+ 	origin = origin.toString();
+ 	if (origin.length > len) 
+ 		return origin.substr(origin.length - len);
+ 	else if (origin.length == len) 
+ 		return origin;
+ 	else {
+ 		var result = '';
+ 		for (var i = 0, max = len - origin.length; i < max; i++) {
+ 			result += prefix;
+ 		}		
+ 		result += origin;
+ 		return result;
+ 	}
+ }
+
+
+ function extend(base, __proto__) {
+ 	if (typeof base === 'object') {
+ 		__proto__ = base;
+ 		base = null;
+ 	}
+
+ 	function F(initializing) {
+ 		if (initializing && typeof initializing === 'boolean') {
+			//Do Nothing
+                    } else {
+                    	this.base && this.base.prototype.constructor.apply(this, arguments);
+                    	this.constructor.apply(this, arguments);
+                    }
+                    return this;
+                }
+
+                if (base) {
+                	F.prototype = new base(true);
+                	F.prototype.base = base;
+                }
+
+                for (var item in __proto__) {
+                	if (__proto__.hasOwnProperty(item)) 
+                		F.prototype[item] = __proto__[item];
+                }
+
+                return F;
+            }
