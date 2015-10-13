27b8b24ce3bd345132a03ee24a00aeb17799e5c7
Josh8909 <josh.luo8909@gmail.com>
2015-10-13 17:52:13 +0800

    Format code

--- js/common.js
+++ js/common.js
@@ -3,45 +3,45 @@
 //       len    数据长度
 //       prefix 补全字符
 function toFixed(origin, len, prefix) {
-	origin = origin.toString();
-	if (origin.length > len)
-		return origin.substr(origin.length - len);
-	else if (origin.length == len)
-		return origin;
-	else {
-		var result = '';
-		for (var i = 0, max = len - origin.length; i < max; i++) {
-			result += prefix;
-		}
-		result += origin;
-		return result;
-	}
+    origin = origin.toString();
+    if (origin.length > len)
+        return origin.substr(origin.length - len);
+    else if (origin.length == len)
+        return origin;
+    else {
+        var result = '';
+        for (var i = 0, max = len - origin.length; i < max; i++) {
+            result += prefix;
+        }
+        result += origin;
+        return result;
+    }
 }
 
 function extend(base, __proto__) {
-	if (typeof base === 'object') {
-		__proto__ = base;
-		base = null;
-	}
+    if (typeof base === 'object') {
+        __proto__ = base;
+        base = null;
+    }
 
-	function F(initializing) {
-		if (initializing && typeof initializing === 'boolean') {
+    function F(initializing) {
+        if (initializing && typeof initializing === 'boolean') {
             // Do Nothing
         } else {
-        	this.base && this.base.prototype.constructor.apply(this, arguments);
-        	this.constructor.apply(this, arguments);
+            this.base && this.base.prototype.constructor.apply(this, arguments);
+            this.constructor.apply(this, arguments);
         }
         return this;
     }
 
     if (base) {
-    	F.prototype = new base(true);
-    	F.prototype.base = base;
+        F.prototype = new base(true);
+        F.prototype.base = base;
     }
 
     for (var item in __proto__) {
-    	if (__proto__.hasOwnProperty(item))
-    		F.prototype[item] = __proto__[item];
+        if (__proto__.hasOwnProperty(item))
+            F.prototype[item] = __proto__[item];
     }
 
     return F;
